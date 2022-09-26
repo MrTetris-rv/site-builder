ac867fdff4623d4e66d10f4763614eacb7b1e4f7
Yulya Zhydkova <sharailee@bk.ru>
2022-09-26 17:35:47 +0200

    init

---
+++ src/model.js
@@ -0,0 +1,46 @@
+import image from './assets/image.jpg';
+import {
+  TextBlock,
+  TitleBlock,
+  ColumnsBlock,
+  ImageBlock,
+} from './classes/blocks';
+export const model = [
+  new TitleBlock('Site builder using js', {
+    tag: 'h2',
+    styles: {
+      background: 'linear-gradient(to right, #c3b6a1, #9d907a)',
+      color: '#fff',
+      padding: '1.5rem',
+      'text-align': 'center',
+    },
+  }),
+  new ImageBlock(image, {
+    styles: {
+      padding: '2rem 0',
+      display: 'flex',
+      'justify-content': 'center',
+    },
+    imageStyles: {
+      width: '500px',
+      height: 'auto',
+    },
+    alt: 'Image',
+  }),
+  new ColumnsBlock(['Something', 'broken ', 'into', 'columns'], {
+    styles: {
+      background: 'linear-gradient(to bottom, #eab521, #c48419)',
+      padding: '2rem',
+      color: '#fff',
+      'font-weight': 'bold',
+    },
+  }),
+  new TextBlock('something', {
+    styles: {
+      background: 'linear-gradient(to left, #726246, #c3b6a1)',
+      padding: '1rem',
+      'font-weight': 'bold',
+      'text-align': 'center',
+    },
+  }),
+];
---
+++ src/utils.js
@@ -0,0 +1,43 @@
+export function row(content, styles = '') {
+  return `<div class="row marginRow" style="${styles}">${content}</div>`;
+}
+
+export function col(content) {
+  return `<div class="col-sm">${content}</div>`;
+}
+
+export function css(styles = {}) {
+  if (typeof styles === 'string') {
+    return styles;
+  }
+  const toString = (key) => `${key}: ${styles[key]}`;
+  return Object.keys(styles).map(toString).join(';');
+}
+
+export function block(type) {
+  let placeholder = '';
+  switch (type) {
+    case 'image':
+      placeholder = 'img url';
+      break;
+    case 'columns':
+      placeholder = '1-st col;2-st col;';
+      break;
+    default:
+      placeholder = 'value';
+      break;
+  }
+  return `
+  <form name="${type}">
+  <h5>${type}</h5>
+  <div class="form-group">
+    <input class="form-control form-control-sm" name="value" placeholder="${placeholder}">
+  </div>
+
+  <div class="form-group">
+    <input class="form-control form-control-sm" name="styles" placeholder="styles">
+  </div>
+  <button type="submit" class="btn btn-primary btn-sm">Add</button>
+</form>
+<hr>`;
+}
